4ea1a507fed495be178276f395e879b4858212a9
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-02-28 03:31:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,2 @@
 def fizzbuzz(int)
-  if int % 5 == 0
-    if int % 3 == 0
-      "Fizzbuzz"
-    else int % 3 != 0
-      "Buzz"
-    end
-  else
-    if int % 3 == 0
-      "Fizz"
-    else
-    end
-  end
 end
-
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
-fizzbuzz(4)
